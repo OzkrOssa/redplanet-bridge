f6191cd8cc7960fb8b55eb095be9efdee4437a22
Oscar Ossa <oskrlex17@gmail.com>
2024-06-19 16:28:51 -0500

    feat: add webhook event struct

--- internal/models/paymentez_models.go
+++ internal/models/paymentez_models.go
@@ -96,3 +96,56 @@ type PaymentRequetsPayload struct {
 	Order     Order  `json:"order"`
 	IPAddress string `json:"ip_address"`
 }
+
+// -----------WebHook---------------------
+type WebhookEvent struct {
+	Transaction WebHookTransaction `json:"transaction,omitempty"`
+	Split       *WebHookSplit      `json:"split,omitempty"`
+	User        *WebHookUser       `json:"user,omitempty"`
+	Card        *WebHookCard       `json:"card,omitempty"`
+}
+
+type WebHookTransaction struct {
+	Status            string `json:"status,omitempty"`
+	OrderDescription  string `json:"order_description,omitempty"`
+	AuthorizationCode string `json:"authorization_code,omitempty"`
+	StatusDetail      string `json:"status_detail,omitempty"`
+	Date              string `json:"date,omitempty"`
+	Message           string `json:"message,omitempty"`
+	ID                string `json:"id,omitempty"`
+	DevReference      string `json:"dev_reference,omitempty"`
+	CarrierCode       string `json:"carrier_code,omitempty"`
+	Amount            string `json:"amount,omitempty"`
+	PaidDate          string `json:"paid_date,omitempty"`
+	Installments      string `json:"installments,omitempty"`
+	LtpID             string `json:"ltp_id,omitempty"`
+	Stoken            string `json:"stoken,omitempty"`
+	ApplicationCode   string `json:"application_code,omitempty"`
+	TerminalCode      string `json:"terminal_code,omitempty"`
+}
+
+type WebHookUser struct {
+	ID    string `json:"id,omitempty"`
+	Email string `json:"email,omitempty"`
+}
+
+type WebHookCard struct {
+	Bin          string `json:"bin,omitempty"`
+	HolderName   string `json:"holder_name,omitempty"`
+	Type         string `json:"type,omitempty"`
+	Number       string `json:"number,omitempty"`
+	Origin       string `json:"origin,omitempty"`
+	FiscalNumber string `json:"fiscal_number,omitempty"`
+}
+
+type WebHookSplit struct {
+	Transaction []WebHookSplitTransactions `json:"transaction,omitempty"`
+}
+
+type WebHookSplitTransactions struct {
+	Installments      string `json:"installments,omitempty"`
+	Amount            string `json:"amount,omitempty"`
+	ID                string `json:"id,omitempty"`
+	ApplicationCode   string `json:"application_code,omitempty"`
+	AuthorizationCode string `json:"authorization_code,omitempty"`
+}
